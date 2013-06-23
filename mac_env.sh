a9e5155c0ff0ad5b87f3a76201794745bf1eebb4
David Streever <dstreever@hortonworks.com>
2013-06-22 23:03:10 -0400

    docs

--- mac_env.sh
+++ mac_env.sh
@@ -1,4 +1,4 @@
-
+HDP_VER=hdp_1.3.0
 HTTP_BASE="http://public-repo-1.hortonworks.com/HDP/centos6/1.x/GA/1.3.0.0/tars/"
 
 TOOLS_BASE="http://public-repo-1.hortonworks.com/HDP/tools/1.3.0.0/"
@@ -10,5 +10,10 @@ DEFAULT_FILES="etc_default"
 LIB_BASE_DIR=/usr/lib
 DEFAULT_DIR=/etc/default
 
-BASE_CONF_DIR=/var/hadoop/local/hdp_1.3.0
+BASE_DIR=/var/hadoop
+HDFS_BASE_DIR=$BASE_DIR/hdfs/localhost
+
+HADOOP_CONF_DIR=$BASE_DIR/local/$HDP_VER
+
+MYSQL_JDBC_JAR=/var/share/jdbc/
 
