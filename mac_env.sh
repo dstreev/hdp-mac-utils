0d61b85494b9b6226bdf6ec4254fc4ae91629fdd
David Streever <dstreever@hortonworks.com>
2013-09-29 19:33:39 -0400

    HDP 1.3.2

--- mac_env.sh
+++ mac_env.sh
@@ -15,14 +15,14 @@
 #   limitations under the License.
 ################################################
 
-HDP_VER=hdp_1.3.0
-HTTP_BASE="http://public-repo-1.hortonworks.com/HDP/centos6/1.x/GA/1.3.0.0/tars/"
+HDP_VER=hdp_1.3.2
+HTTP_BASE="http://public-repo-1.hortonworks.com/HDP/centos6/1.x/updates/1.3.2.0/tars/"
 
-TOOLS_BASE="http://public-repo-1.hortonworks.com/HDP/tools/1.3.0.0/"
-COMPANION_FILE="hdp_manual_install_rpm_helper_files-1.3.0.1.3.0.0-107"
+# TOOLS_BASE="http://public-repo-1.hortonworks.com/HDP/tools/1.3.2.0/"
+# COMPANION_FILE="hdp_manual_install_rpm_helper_files-1.3.0.1.3.0.0-107"
 
-DEFAULT_FILES_BASE="https://raw.github.com/dstreev/HDP_1.3.0_Mac_Utils/master/"
-DEFAULT_FILES="etc_default"
+# DEFAULT_FILES_BASE="https://raw.github.com/dstreev/HDP_1.3.0_Mac_Utils/master/"
+# DEFAULT_FILES="etc_default"
 
 LIB_BASE_DIR=/usr/lib
 DEFAULT_DIR=/etc/default
